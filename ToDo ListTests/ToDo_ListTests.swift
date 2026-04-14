bcaa9088bcaf3ab3874be5ba88615c7c9cc4660d
Kockstik <kostyadi04@mail.ru>
2026-04-14 18:39:42 +0700

    Initial Commit

---
+++ ToDo ListTests/ToDo_ListTests.swift
@@ -0,0 +1,17 @@
+//
+//  ToDo_ListTests.swift
+//  ToDo ListTests
+//
+//  Created by Diesperov Konstantin on 14.04.2026.
+//
+
+import Testing
+@testable import ToDo_List
+
+struct ToDo_ListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
