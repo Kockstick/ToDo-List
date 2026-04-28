f59e7efe4df0e09acdc999016d4e57d88d489acb
Kockstik <kostyadi04@mail.ru>
2026-04-28 19:38:45 +0700

    Add formattedDate to Date

--- ToDo List/Entity/ToDo.swift
+++ ToDo List/Entity/ToDo.swift
@@ -15,3 +15,15 @@ struct ToDo: Codable {
     var completed: Bool
     var date: Date? = Date.now
 }
+
+extension Date{
+    private static let formatter: DateFormatter = {
+        let f = DateFormatter()
+        f.dateFormat = "dd/MM/yy"
+        return f
+    }()
+    
+    var formattedDate: String {
+        Date.formatter.string(from: self)
+    }
+}
