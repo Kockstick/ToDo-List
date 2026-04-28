cdf4ef1d3d0b7859c18b975baf44c9755bd89f4b
Kockstik <kostyadi04@mail.ru>
2026-04-28 19:38:09 +0700

    Fix Entity

--- ToDo List/Entity/ToDoEntity.swift
+++ ToDo List/Entity/ToDoEntity.swift
@@ -1,5 +1,5 @@
 //
-//  ToDo+CoreDataClass.swift
+//  ToDoEntity+CoreDataClass.swift
 //  ToDo List
 //
 //  Created by Diesperov Konstantin on 28.04.2026.
@@ -7,30 +7,10 @@
 //
 
 import Foundation
-import CoreData
-
-@objc(ToDoEntity)
-public final class ToDoEntity: NSManagedObject {
-
-}
 
 extension ToDoEntity {
-    @nonobjc public class func fetchRequest() -> NSFetchRequest<ToDoEntity> {
-        return NSFetchRequest<ToDoEntity>(entityName: "ToDo")
-    }
-
-    @NSManaged public var completed: Bool
-    @NSManaged public var date: Date?
-    @NSManaged public var id: Int32
-    @NSManaged public var title: String?
-    @NSManaged public var todo: String
-    @NSManaged public var userId: Int32
-
-}
-
-extension ToDoEntity : Identifiable {
     func toModel() -> ToDo {
-        ToDo(id: id, title: title, todo: todo, userId: userId, completed: completed, date: date)
+        ToDo(id: id, title: title, todo: todo ?? "", userId: userId, completed: completed, date: date)
     }
     
     func update(from model: ToDo) {
