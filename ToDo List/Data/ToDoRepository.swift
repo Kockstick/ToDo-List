d47d39b3ee782829065446a873bd6f9ec417989e
Kockstik <kostyadi04@mail.ru>
2026-04-30 12:48:48 +0700

    Fix repository for Core Data

--- ToDo List/Data/ToDoRepository.swift
+++ ToDo List/Data/ToDoRepository.swift
@@ -5,45 +5,77 @@
 //  Created by Diesperov Konstantin on 28.04.2026.
 //
 
+import CoreData
+import Network
+
 class ToDoRepository: IToDoRepository {
-    var delegate: ToDoRepositoryDelegate?
+    static var shared: IToDoRepository = ToDoRepository()
+    var delegate: ToDoRepositoryDelegate!
     
-    static var shared = ToDoRepository()
+    let monitor = NWPathMonitor()
+    let queue = DispatchQueue(label: "Monitor")
     
     private(set) var loading = false {
         didSet{
+            print("Loading: \(loading)")
             delegate?.onLoadStateChange(loading)
         }
     }
     
-    var todos: [ToDo] = []
-    
-    private init(){
-        preload()
+    private init() {
+        monitor.pathUpdateHandler = { [weak self] path in
+            if path.status == .satisfied {
+                self?.preload()
+            }
+        }
+        monitor.start(queue: queue)
     }
     
-    private func preload() {
+    func preload() {
+        guard !loading else { return }
+        print("Try preload data from API")
         Task{
             do {
                 loading = true
                 defer { loading = false }
                 if let data = try await DataPreloader.shared.preloadDataIfNeeded() {
-                    todos += data
+                    for todo in data {
+                        addToDo(todo: todo)
+                    }
                 }
             } catch {
                 print(error.localizedDescription)
             }
         }
     }
+    
+    func addToDo(todo: ToDo) {
+        CoreDataStack.shared.viewContext.performAndWait {
+            let context = CoreDataStack.shared.viewContext
+
+            let entity = ToDoEntity(context: context)
+            entity.id = todo.id
+            entity.title = todo.title
+            entity.todo = todo.todo
+            entity.date = todo.date
+            entity.userId = todo.userId
+            entity.completed = false
+
+            do {
+                try context.save()
+            } catch {
+                print("Save error:", error)
+            }
+        }
+    }
 }
 
-protocol IToDoRepository {
+protocol IToDoRepository: AnyObject {
     var loading: Bool { get }
-    var todos: [ToDo] { get set }
-    var delegate: ToDoRepositoryDelegate? { get set }
+    var delegate: ToDoRepositoryDelegate! { get set }
+    func preload()
 }
 
 protocol ToDoRepositoryDelegate: AnyObject {
-    func toDoDidUpdate(_ todos: [ToDo])
     func onLoadStateChange(_ loading: Bool)
 }
